e814166b061c1c22e227d26286b29a3c077028b4
Souvik Sadhu <souviksadhu052@gmail.com>
2026-09-07 23:34:31 +0530

    Add multiple ifalse Practice code

---
+++ multipleifalse.py
@@ -0,0 +1,16 @@
+a = int(input("enter your age : "))
+
+if(a>=18):
+    if(a%2==0):
+        print("even")
+    else:
+        print("odd")
+if(a==0):
+    print("you are mad change your age")
+    
+else:
+    if(a<0):
+        print("you are entering wrong age which is invalid")
+    else:
+        print("okkkk")
+    
