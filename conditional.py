565bbb0b91ca88c7a0732a99d3a7df37f9798353
Souvik Sadhu <souviksadhu052@gmail.com>
2026-09-07 23:32:04 +0530

    Add Condition Practice code

---
+++ conditional.py
@@ -0,0 +1,18 @@
+a = int(input("Enter your age: "))
+
+# id elif else ladder
+
+if(a>=18):
+    print("You are adult")
+    print("good for understanding programming")
+
+elif(a<0):
+    print("you are entering a wrong negative age")
+
+elif(a==0):
+    print("you are entering a wrong age which is invalid")
+
+else:
+    print("you are not a adult")
+
+print("end of program")
