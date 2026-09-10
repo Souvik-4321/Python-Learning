afb26b7e1ebbf3bf0334a099dab31c3cbeb0e0e1
Souvik Sadhu <souviksadhu052@gmail.com>
2026-09-10 14:02:48 +0530

    Add more Practice code

---
+++ break_continue_pass.py
@@ -0,0 +1,20 @@
+#Break statement using for loop
+i= 0
+for i in range(0,100):
+    if(i==51):
+        break
+    print(i)
+
+#continue statement using for loop
+for i in range(101):
+    if(i<=90 and i>=50):
+        continue
+    print(i)
+
+#pass statement using for loop
+for i in range(700):
+    pass
+i = 0
+while i in range(40):
+    print(i)
+    i+=1
