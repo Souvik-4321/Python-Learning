5c5d01fdefc51a9252d6b9c032d28ea2388e8642
Souvik Sadhu <souviksadhu052@gmail.com>
2026-09-12 13:41:05 +0530

    Add more Practice code

---
+++ practice_1.py
@@ -0,0 +1,20 @@
+#Multipication table using for loop
+n = int(input("Enter a number: "))
+for i in range(1,11):
+    print(f"{n} x {i} = {n*i}")
+
+# Multipication table using while loop
+m = int(input("Enter a number: "))
+i = 1
+while(i<11):
+    print(f"{m} x {i} = {m*i}")
+    i+=1
+
+# Reverse Multipication table
+n = int(input("Enter a number: "))
+for i in range(1,11):
+ print(f"{n} x {11-i} = {n*(11-i)}")
+
+
+
+
